473209fa207d5a3fb55050d0038b2e970930fcfc
mentejoao <skhulzxd@gmail.com>
2024-11-22 10:15:04 -0300

    feat: endpoint for recommendation based on content

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI, Query
+from typing import Optional, List
+from pydantic import BaseModel
+from models.recommender import Recommender
 
 app = FastAPI()
 
+recommender = Recommender()
+
 @app.get("/")
-def root():
+async def root():
     return {"message": "Bem-vindo à API de Recomendações!"}
+
+@app.get("/recommendations/content")
+async def get_recommendations(query: str = Query(..., description="Texto para recomendar"), top_k: int = 20):
+    """
+    endpoint para obter recomendações baseadas na barra de pesquisa, de acordo com o nome.
+    """
+#    recommendations = recommender.recommend(query, top_k)
+    return {
+        "input_query": query,
+#        "recommendations": recommendations,
+#        "count": len(recommendations),
+    }
 
 
 if __name__ == "__main__":
