405ab7c7fae29264da7dc7ad26e174eb8d24465e
mentejoao <skhulzxd@gmail.com>
2024-11-29 17:48:43 -0300

    fix: alterando recommendation_user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request, Query
 from typing import Optional, List
 from pydantic import BaseModel
 from models.recommender import Recommender
@@ -24,21 +24,29 @@ async def get_recommendations(query: str = Query(..., description="Texto para re
     }
 
 @app.get("/recommendations/user")
-async def get_recommendations_user(): 
+async def get_recommendations_user(request: Request):
     """            
     user_content: possui avaliacao media de 14 generos
     dimensoes user_content: 14 
     user_vectir>                                                                                          
     endpoint para obter recomendações baseadas nas avaliações médias do usuário.
     """
-#    recommendations = recommender.recommend(query, top_k)
+    ## pegar dados do json e colocar em user_vec
+    user_data = await request.json()
+    
+    # Separar 'top_k' e criar o vetor 'user_vec'
+    top_k = user_data.pop("top_k", None)
+    user_vec = list(user_data.values())
+
+    # Chamar o sistema de recomendação
+    recommendations = recommender.recommend_user(user_vec, top_k)
+    
     return {
-#        "recommendations": recommendations,
-#        "count": len(recommendations),
+        "recommendations": recommendations
     }
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, debug=True, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
