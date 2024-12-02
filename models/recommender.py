c6f00bd3d9b3e11124c6f19b0c973954790acec4
mentejoao <skhulzxd@gmail.com>
2024-12-02 13:52:06 -0300

    fix: arrumando scaler

--- models/recommender.py
+++ models/recommender.py
@@ -12,6 +12,16 @@ import json
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="%(asctime)s - %(levelname)s - %(message)s",
+#     handlers=[
+#         logging.StreamHandler(), 
+#         logging.FileHandler("./logs/recommender.log", mode="a")
+#     ]
+# )
+
+
 
 class L2Normalize(tf.keras.layers.Layer):
     def __init__(self, axis=1, **kwargs):
@@ -31,15 +41,6 @@ class L2Normalize(tf.keras.layers.Layer):
         return cls(**config)
     
 
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s - %(levelname)s - %(message)s",
-#     handlers=[
-#         logging.StreamHandler(), 
-#         logging.FileHandler("./logs/recommender.log", mode="a")
-#     ]
-# )
-
 models = {
     "recUser": "./notebooks/best_model.keras",
 }
@@ -53,11 +54,16 @@ class Recommender:
         self.scaler = MinMaxScaler((-1, 1))
         self.scaledata = True
 
-        self.item_vecs, self.movie_dict = self.load_data()
+        self.item_train, self.user_train, self.y_train, self.item_vecs, self.movie_dict = self.load_data()
+
+        self.scalerItem.fit(self.item_train)
+        self.scalerUser.fit(self.user_train)
+        self.scaler.fit(self.y_train.reshape(-1, 1))
+        
         for model_name, model_path in models.items():
             self.load_models(model_name, model_path)
 
-    def get_pred_movies_json(y_p, user, item, movie_dict, top_k):
+    def get_pred_movies_json(self, y_p, user, item, movie_dict, top_k):
         """ Return results of prediction of a new user in JSON format. Inputs are expected
             to be in sorted order, unscaled. """
         count = 0
@@ -65,28 +71,32 @@ class Recommender:
         results = []
 
         for i in range(0, y_p.shape[0]):
-            if count == maxcount:
+            if count == top_k:
                 break
-            movie_id = item[i, 0].astype(int)
+            movie_id = int(item[i, 0])  # Garantir que seja do tipo int
             if movie_id in movies_listed:
                 continue
             movies_listed[movie_id] = 1
             count += 1
-            
+
             results.append({
-                "y_p": float(y_p[i, 0]),  # serialize
                 "movie_id": movie_id,
-                "rating_ave": float(item[i, 2]),
+                "y_p": float(y_p[i, 0]),  # Garantir que seja serializável
                 "title": movie_dict[movie_id]['title'],
                 "genres": movie_dict[movie_id]['genres']
             })
 
-        return json.dumps(results, indent=4)
+    # Encapsula o resultado dentro de "recommendations_users"
+        return json.dumps({"recommendations_users": results}, indent=4) 
 
         
     def load_data(self):
+        item_train = genfromtxt('./notebooks/data/content_item_train.csv', delimiter=',')
+        user_train = genfromtxt('./notebooks/data/content_user_train.csv', delimiter=',')
+        y_train    = genfromtxt('./notebooks/data/content_y_train.csv', delimiter=',')
+
         item_vecs = genfromtxt('./notebooks/data/content_item_vecs.csv', delimiter=',')
-       
+        
         movie_dict = defaultdict(dict)
         count = 0
     #    with open('./data/movies.csv', newline='') as csvfile:
@@ -102,7 +112,7 @@ class Recommender:
                     movie_dict[movie_id]["title"] = line[1]  
                     movie_dict[movie_id]["genres"] =line[2]  
 
-        return(item_vecs, movie_dict)
+        return(item_train, user_train, y_train, item_vecs, movie_dict)
 
     def load_models(self, model_name: str, model_path: str):
         if model_name not in self.__models:
@@ -129,20 +139,19 @@ class Recommender:
         return(user_vecs)
     
     # predict on  everything, filter on print/use
-    def predict_uservec(self, user_vecs, item_vecs, model, u_s, i_s, scaler, ScalerUser, ScalerItem, scaledata=False):
+    def predict_uservec(self, user_vecs, item_vecs, scaler, ScalerUser, ScalerItem, scaledata=False):
         """ given a user vector, does the prediction on all movies in item_vecs returns
             an array predictions sorted by predicted rating,
             arrays of user and item, sorted by predicted rating sorting index
         """
         if scaledata:
-            self.scalerUser.fit(user_vecs)
-            self.scalerItem.fit(item_vecs)
+            print(f"user_vecs dentro de predict {user_vecs}")
             scaled_user_vecs = ScalerUser.transform(user_vecs)
             scaled_item_vecs = ScalerItem.transform(item_vecs)
-            y_p = self.model.predict([scaled_user_vecs[:, u_s:], scaled_item_vecs[:, i_s:]])
+            y_p = self.model.predict([scaled_user_vecs[:, 3:], scaled_item_vecs[:, 1:]])
+            print(y_p)
         else:
-            y_p = self.model.predict([user_vecs[:, u_s:], item_vecs[:, i_s:]])
-        self.scaler.fit(y_p.reshape(-1, 1))
+            y_p = self.model.predict([user_vecs[:, 3:], item_vecs[:, 1:]])
         y_pu = scaler.inverse_transform(y_p)
 
         if np.any(y_pu < 0) : 
@@ -151,11 +160,11 @@ class Recommender:
         sorted_ypu   = y_pu[sorted_index]
         sorted_items = item_vecs[sorted_index]
         sorted_user  = user_vecs[sorted_index]
-        return(sorted_index, sorted_ypu, sorted_items, sorted_user)
+        return(sorted_ypu, sorted_items, sorted_user)
     
-    def recommend_user(self, user_vect, count: int = 10) -> str:
-        self.user_vec = self.gen_user_vecs(user_vect, len(self.item_vecs))
-        self.sorted_index, self.sorted_ypu, self.sorted_items, self.sorted_user = self.predict_uservec(self.user_vec, self.item_vecs, self.__models, u_s=3, i_s=1, scaler=self.scaler, ScalerUser=self.scalerUser, ScalerItem=self.scalerItem, scaledata=self.scaledata)                                                
-        json_recommendation = self.get_pred_movies_json(self.sorted_ypu, self.sorted_user, self.sorted_items, self.movie_dict, top_k=10)
+    def recommend_user(self, user_vect, count) -> str:
+        user_vec = self.gen_user_vecs(user_vect, len(self.item_vecs))
+        sorted_ypu, sorted_items, sorted_user = self.predict_uservec(user_vec, self.item_vecs, scaler=self.scaler, ScalerUser=self.scalerUser, ScalerItem=self.scalerItem, scaledata=self.scaledata)                                              
+        json_recommendation = self.get_pred_movies_json(sorted_ypu, sorted_user, sorted_items, self.movie_dict, count)
 
         return json_recommendation
