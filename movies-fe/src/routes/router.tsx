a59f6901f5207c17edd503a063e9490b93444dc2
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2024-12-03 21:51:59 -0300

    chore(filmes): criação das páginas de filmes recomendados

--- movies-fe/src/routes/router.tsx
+++ movies-fe/src/routes/router.tsx
@@ -2,12 +2,18 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/templates/Layout';
 import Home from '../components/pages/Home';
 import Login from '../components/pages/Login';
+import RecommendedList from '../components/pages/RecommendedList';
+import MovieView from '../components/pages/MovieView';
 
 export const routes = createBrowserRouter([
   {
     path: 'assista-ai',
     element: <Layout />,
-    children: [{ path: '', element: <Home /> }],
+    children: [
+      { path: '', element: <Home /> },
+      { path: 'recomendados', element: <RecommendedList /> },
+      { path: 'filmes/:id', element: <MovieView /> },
+    ],
   },
   { path: 'login', element: <Login /> },
 ]);
