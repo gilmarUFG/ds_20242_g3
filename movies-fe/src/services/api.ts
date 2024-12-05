940bfaf5a77fefbd5eb91700cd45a82f481d92a4
amadeulee <amadeulee@discente.ufg.br>
2024-12-05 17:39:19 -0300

    adicionar ip que esta rodando o back

--- movies-fe/src/services/api.ts
+++ movies-fe/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://18.116.47.16:8080',
   headers: {
     'Content-Type': 'application/json',
   },
